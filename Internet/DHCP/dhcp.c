ab3da227f3434c3db4498ccfb0c8b9fcc260a645
SMMB-DEV <13986458+SMMB-DEV@users.noreply.github.com>
2024-07-25 10:07:03 +0330

    Minor changes

--- Internet/DHCP/dhcp.c
+++ Internet/DHCP/dhcp.c
@@ -50,7 +50,7 @@
 //
 //*****************************************************************************
 
-#include "socket.h"
+#include "..\..\Ethernet\socket.h"
 #include "dhcp.h"
 
 /* If you want to display debug & processing message, Define _DHCP_DEBUG_ in dhcp.h */
